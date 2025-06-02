cd8f29c2d120dfb51beec4bca81fb2c9e7dd78bb
Vitor R <vitorferraz12r@gmail.com>
2025-06-02 13:39:46 -0300

    primeiro commit

---
+++ src/scripts/objects/screen.js
@@ -0,0 +1,27 @@
+const screen = {
+  userProfile: document.querySelector(".profile-data"),
+  renderUser(user) {
+    this.userProfile.innerHTML = `<div class="info">
+                <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
+                <div class="data">
+                    <h1>${user.name ?? "não possui nome cadastrado 😭"}</h1>
+                    <p>${user.bio ?? "não possui bio cadastrado 😭"}</p>
+                </div>
+                </div>`;
+
+    let repositoriesItens = "";
+    user.repositories.forEach((repo) => (repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`));
+
+    if (user.repositories.length > 0) {
+      this.userProfile.innerHTML += `<div class="repositories section">
+                                        <h2>Repositórios</h2>
+                                        <ul>${repositoriesItens}</ul>
+                                     </div>`;
+    }
+  },
+  renderNotFound() {
+    this.userProfile.innerHTML = `<h3>Usuário não encontrado</h3>`;
+  },
+};
+
+export { screen };
